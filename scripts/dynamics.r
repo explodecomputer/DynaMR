0ccfc772ba30048dd714c47c231fa96e887b7b2e
Ildar Sadreev <i.sadreev@bristol.ac.uk>
2021-03-05 12:39:30 +0000

    added rmd file

--- scripts/dynamics.r
+++ scripts/dynamics.r
@@ -1,1 +1,41 @@
-dsffas
+###########################
+
+#solving an ode
+
+ 
+
+require(deSolve)
+
+ 
+
+ 
+
+expgrowth=function(Time,State,Params){
+
+                with(as.list(c(State,Params)),{
+
+                                                                dy=a*y
+
+                                                                dx=b*y
+
+                                                                res<-list(c(dy,dx))
+
+                                                                return(res)
+
+                                                })
+
+}
+
+ 
+
+#use documentation for plotting odes here:
+
+#http://www.inside-r.org/packages/cran/deSolve/docs/hist.deSolve
+
+ 
+
+out<-ode(func=expgrowth,y=c(y=5,x=0),parms=c(a=1.5,b=-1),times=seq(0, 5, by = .1))
+
+ 
+
+###
